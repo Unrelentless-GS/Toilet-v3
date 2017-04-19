7e2372dc73b3569d31ee4d9f00bafeeaae15634d
Pavel Boryseiko <pavel.boryseiko@gridstone.com.au>
2017-04-19 14:23:52 +1000

    Initial Commit

---
+++ Toilet-v3/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Toilet-v3
+//
+//  Created by Pavel Boryseiko on 19/4/17.
+//  Copyright © 2017 GRIDSTONE. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Toilet-v3/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Toilet-v3
+//
+//  Created by Pavel Boryseiko on 19/4/17.
+//  Copyright © 2017 GRIDSTONE. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
